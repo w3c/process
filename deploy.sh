baa5a5d4f14a09895d09b13496080792f10620d6
Florian Rivoal <git@florian.rivoal.net>
2019-04-26 11:40:22 +0900

    Remove debug-only tweak to the build script

--- deploy.sh
+++ deploy.sh
@@ -70,7 +70,7 @@ fi
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
-echo git commit -m "Deploy to GitHub Pages: ${SHA} from branch \"${TRAVIS_BRANCH}\""
+git commit -m "Deploy to GitHub Pages: ${SHA} from branch \"${TRAVIS_BRANCH}\""
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
