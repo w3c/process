391802f439092dd12494e2ce6419378a96933dea
Florian Rivoal <git@florian.rivoal.net>
2019-05-20 22:21:59 +0900

    fix branch names in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 # List of long-lived topic branch names to be published on github.io as a subdirectory
-TOPIC_BRANCHES=("evergreen director-free")
+TOPIC_BRANCHES=("evergreen" "director-free")
 
 containsElement () {
   local e match="$1"
