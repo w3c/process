5d32e4cd9a5aa2bb5a922d2715fb8a291c7a959a
plehegar <plh@w3.org>
2021-06-23 18:20:35 -0400

    Improves user.email

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,8 @@ set -x
 # set up the github credentials
 
 # GITHUB_EVENT_PATH is a JSON file so ...this is a bit of a hack...
-COMMIT_AUTHOR_EMAIL=`cat $GITHUB_EVENT_PATH | grep email | head -n 1 | cut -d ":" -f2 | cut -d '"' -f2`
+PULL_AUTHOR_EMAIL=`cat $GITHUB_EVENT_PATH | grep email | head -n 1 | cut -d ":" -f2 | cut -d '"' -f2`
+COMMIT_AUTHOR_EMAIL=${PULL_AUTHOR_EMAIL:-"github-action@users.noreply.github.com"}
 
 git config --global user.email $COMMIT_AUTHOR_EMAIL
 git config --global user.name $GITHUB_ACTOR
