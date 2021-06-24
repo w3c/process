679f37e136a157b8bc27e3d0dca286c07d3926b3
plehegar <plh@w3.org>
2021-06-24 08:45:34 -0400

    Use the username 'w3cbot' to interact with GitHub

--- deploy.sh
+++ deploy.sh
@@ -34,12 +34,8 @@ set -x
 
 # set up the github credentials
 
-# GITHUB_EVENT_PATH is a JSON file so ...this is a bit of a hack...
-PULL_AUTHOR_EMAIL=`cat $GITHUB_EVENT_PATH | grep email | head -n 1 | cut -d ":" -f2 | cut -d '"' -f2`
-COMMIT_AUTHOR_EMAIL=${PULL_AUTHOR_EMAIL:-"github-action@users.noreply.github.com"}
-
-git config --global user.email $COMMIT_AUTHOR_EMAIL
-git config --global user.name $GITHUB_ACTOR
+git config --global user.email noreply@unknown.w3.org
+git config --global user.name w3cbot
 git config --global user.password $GITHUB_TOKEN
 
 REPO_URL="https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
